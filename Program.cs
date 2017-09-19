abf2ea0e655bb52cb42bf762e499b7e9c28aa2f6
MattMcLennan <mclennan6891@gmail.com>
2017-09-19 08:46:56 -0400

    Ignore empty and comment lines added to Parser

--- Program.cs
+++ Program.cs
@@ -1,12 +1,72 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace HackAssembler
 {
-    class Program
+
+    enum ExitCode : int
     {
-        static void Main(string[] args)
+        Success = 0,
+        InvalidFilename = 1,
+        UnknownError = 10
+    }
+
+    class Parser
+    {
+        public static List<string> ParseFile(string inputFile)
         {
-            Console.WriteLine("Hello World!");
+            var result = new List<string>();
+
+            foreach (var line in File.ReadLines(@inputFile))
+            {
+                if (IsEmptyLine(line) || IsCommentLine(line))
+                {
+                    continue;
+                }
+
+                result.Add(line);
+            }
+
+            return result;
+        }
+
+        private static bool IsEmptyLine(string line)
+        {
+            return string.IsNullOrWhiteSpace(line);
+        }
+
+        private static bool IsCommentLine(string line)
+        {
+            return line.StartsWith("//");
+        }
+
+        private static void UnpackInstruction(string line)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void InsertIntoField(string line)
+        {
+            throw new NotImplementedException();
         }
     }
+
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            string inputFile = args[0];
+            if (!File.Exists(inputFile))
+            {
+                return (int)ExitCode.InvalidFilename;
+            }
+
+            Parser.ParseFile(inputFile);
+
+            return (int)ExitCode.Success;
+        }
+    }
+
+
 }
